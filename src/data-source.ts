8763d4a5fb7fa49d9230b945bbeac730d4829f0b
yjkdev <dustmqgksmsdev24@gmail.com>
2025-02-23 00:36:25 +0900

    ✨ feat : Entity, Migrastion 설정 완료

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,20 +1,18 @@
-/* import { DataSource } from 'typeorm';
+import { DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
-import path from 'path';
 
 dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: process.env.DB_HOST || 'db',
+  host: process.env.DB_HOST || (process.env.NODE_ENV === 'production' ? 'db' : 'localhost'),
   port: Number(process.env.DB_PORT) || 3306,
   username: process.env.DB_USERNAME || 'root',
   password: process.env.DB_PASSWORD || 'Mydb1234@',
   database: process.env.DB_DATABASE || 'capstone',
-  entities: [path.resolve(__dirname, '**', '*.entity.{ts,js}')],
-  migrations: [path.resolve(__dirname, 'migrations', '*.ts')],
-  migrationsTableName: 'migrations_history',
+  entities: [process.env.NODE_ENV === 'production' ? 'dist/**/*.entity.js' : 'src/**/*.entity.ts'],
+  migrations: [process.env.NODE_ENV === 'production' ? 'dist/migrations/*.js' : 'src/migrations/*.ts'],
   synchronize: false,
   logging: true,
+  extra: { connectTimeout: 60000 },
 });
- */
