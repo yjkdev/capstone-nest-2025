d20035bdb54bb72e782535547fd457a6f722b6db
EUN YEONG MIN <qlafqjsgh123@naver.com>
2025-02-25 17:05:37 +0900

    ✨ feat : DB기초설정 메인병함함

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 4000);
+  await app.listen(process.env.PORT ?? 4001);
 }
 bootstrap();
