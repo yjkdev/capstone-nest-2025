49537f5222db207c8976ead053c3aaad950d3d17
yjkdev <dustmqgksmsdev24@gmail.com>
2025-03-04 10:28:20 +0900

    ✨ feat : 단어장수정

--- src/words/words.module.ts
+++ src/words/words.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Word } from './entities/words.entity';
+import { WordBook } from './entities/word-books.entity';
+import { WordMiddle } from './entities/word-middle.entity';
 import { WordsService } from './words.service';
 import { WordsController } from './words.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Word])],
+  imports: [TypeOrmModule.forFeature([Word, WordBook, WordMiddle])],
   providers: [WordsService],
   controllers: [WordsController],
 })
